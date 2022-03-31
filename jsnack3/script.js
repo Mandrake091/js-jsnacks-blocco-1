37a6f70c0cb5584e4560491b105840af8bf3142a
Mandrake091 <wasdgamer91@gmail.com>
2022-03-31 20:46:34 +0200

    end exercise

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,31 +1,30 @@
 button = document.getElementById("button").addEventListener("click", total);
 
-let input = document.getElementById('input');
-
-
-
 let h1 = document.getElementById('result');
 
+function total(){
 
-
-
-
-
-
-
-
-function total() {
-    let singleNumbers = input.value.split('');
-
-    const initialValue = 0;
-    const sumWithInitial = singleNumbers.reduce(
-        (previousValue, currentValue) => previousValue + currentValue,
-        initialValue
-    );
-
-    console.log(sumWithInitial);
-
-
-
-
+let numbers = false;
+while (numbers == false) {
+   let input = document.getElementById('input').value;
+    if (input.length != 4) {
+        alert("Inserisci un numero di 4 cifre!");
+        return
+    } else {
+        numbers = true;
+        const numberArray = input.split("");
+        let results = 0;
+        console.log(numberArray)
+        let i = 0
+        while (i < 4) {
+            results += parseInt(numberArray[i]);
+            i++;
+        }
+        h1.innerText=(`La somma delle cifre inserite è ${results}`)
+    }
 }
+}
+
+
+
+
